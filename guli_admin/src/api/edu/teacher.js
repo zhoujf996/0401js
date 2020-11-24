2af605719666727c60fe7c00803d56bc022da514
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-11-24 12:03:53 +0800

    first commit

---
+++ guli_admin/src/api/edu/teacher.js
@@ -0,0 +1,19 @@
+import request from '@/utils/request'
+
+const teacher_api = '/teacher'
+
+export default {
+  getList() {
+    return request({
+      url: '/teacher/list',
+      method: 'get'
+    })
+  },
+  getPageList(page, limit, searchObj) {
+    return request({
+      url: `${teacher_api}/${page}/${limit}`,
+      method: 'post',
+      data: searchObj
+    })
+  }
+}
