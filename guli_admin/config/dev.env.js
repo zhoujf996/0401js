968ec2aaa2707853ba93526e90e9e5e11a56ec66
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-11-26 11:36:29 +0800

    前端实现导入课程分类

--- guli_admin/config/dev.env.js
+++ guli_admin/config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:9000"'
+  BASE_API: '"http://localhost:9000"',
+  OSS_PATH: '"https://guli-1124.oss-cn-shenzhen.aliyuncs.com"'
 })
