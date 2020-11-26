32fe2907f9088db4d5e481e0bb42cbc7521e1f22
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-11-26 14:11:37 +0800

    课程分类前端列表显示

---
+++ guli_admin/src/api/edu/subject.js
@@ -0,0 +1,14 @@
+import request from '@/utils/request'
+
+const api_name = '/subject'
+
+export default{
+
+  getNestedTreeList() {
+    return request({
+      url: `${api_name}/tree`,
+      method: 'get'
+    })
+  }
+
+}
