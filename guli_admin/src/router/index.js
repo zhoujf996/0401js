294ea245259d3ff4ca7904f30756d6613aa956f9
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-11-26 20:52:58 +0800

    课程管理-前端样式实现

--- guli_admin/src/router/index.js
+++ guli_admin/src/router/index.js
@@ -87,6 +87,48 @@ export const constantRouterMap = [
     ]
   },
   {
+    path: '/course',
+    component: Layout,
+    redirect: '/course/list',
+    name: '课程管理',
+    meta: { title: '课程管理', icon: 'example' },
+    children: [
+      {
+        path: 'list',
+        name: '课程列表',
+        component: () => import('@/views/course/list'),
+        meta: { title: '课程列表', icon: 'table' }
+      },
+      {
+        path: '/info',
+        name: '发布课程',
+        component: () => import('@/views/course/info'),
+        meta: { title: '发布课程', icon: 'tree' }
+      },
+      {
+        path: 'info/:id',
+        name: 'EduCourseInfoEdit',
+        component: () => import('@/views/course/info'),
+        meta: { title: '编辑课程基本信息', noCache: true },
+        hidden: true
+      },
+      {
+        path: 'chapter/:id',
+        name: 'EduCourseChapterEdit',
+        component: () => import('@/views/course/chapter'),
+        meta: { title: '编辑课程大纲', noCache: true },
+        hidden: true
+      },
+      {
+        path: 'publish/:id',
+        name: 'EduCoursePublishEdit',
+        component: () => import('@/views/course/publish'),
+        meta: { title: '发布课程', noCache: true },
+        hidden: true
+      }
+    ]
+  },
+  {
     path: '/example',
     component: Layout,
     redirect: '/example/table',
